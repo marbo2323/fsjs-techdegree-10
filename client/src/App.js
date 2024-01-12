bcd80ceac608d501741b830ad529c2e62c3f219d
Arbo Lindma <arbo.lindma@telia.ee>
2024-01-12 18:35:38 +0200

    add app styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,35 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import { useEffect, useState } from "react";
-
+import Courses from "./components/Courses";
+import Header from "./components/Header";
 function App() {
   const [courses, setCourses] = useState([]);
 
-  async function getCourses() {
+  const fetchCourses = async () => {
+    console.log("fetchCourses");
     const url = "http://localhost:5000/api/courses";
     const fetchOptions = {
       method: "GET",
     };
     const result = await fetch(url, fetchOptions);
-    const courses = await result.json();
-    console.log();
-    setCourses(courses);
-  }
+    const data = await result.json();
+    setCourses(data);
+  };
 
   useEffect(() => {
-    getCourses();
-  });
+    console.log("useEffect");
+    fetchCourses();
+  }, []);
 
   return (
-    <div className="App">
-      <ul>
-        {courses.map((course, i) => (
-          <li key={course.id}>{course.title}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Courses courses={courses} />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
