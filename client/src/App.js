dc31f0e2a10357dc8e7a59ab3b56346b4945dd7e
Arbo Lindma <arbo.lindma@telia.ee>
2024-01-12 15:11:07 +0200

    test calling api

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [courses, setCourses] = useState([]);
+
+  async function getCourses() {
+    const url = "http://localhost:5000/api/courses";
+    const fetchOptions = {
+      method: "GET",
+    };
+    const result = await fetch(url, fetchOptions);
+    const courses = await result.json();
+    console.log();
+    setCourses(courses);
+  }
+
+  useEffect(() => {
+    getCourses();
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {courses.map((course, i) => (
+          <li key={course.id}>{course.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
