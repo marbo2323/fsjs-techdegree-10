3fa3dc4af7f87ba06ee3e6b6850c60d1219f341c
Arbo Lindma <arbo.lindma@telia.ee>
2024-01-15 10:00:20 +0200

    change sessionStorage to localStorage

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -3,16 +3,16 @@ import { signIn } from "../utils/apiClient";
 const UserContext = createContext(null);
 
 export const UserProvider = (props) => {
-  const getSessionItem = (key) => {
-    const item = sessionStorage.getItem(key);
+  const getStorageItem = (key) => {
+    const item = localStorage.getItem(key);
     if (item) {
       return JSON.parse(item);
     }
     return null;
   };
 
-  const [authUser, setAuthUser] = useState(getSessionItem("authUser"));
-  const [credentials, setCredentials] = useState(getSessionItem("credentials"));
+  const [authUser, setAuthUser] = useState(getStorageItem("authUser"));
+  const [credentials, setCredentials] = useState(getStorageItem("credentials"));
 
   const signInUser = async (credentials) => {
     const response = await signIn(credentials);
@@ -21,8 +21,8 @@ export const UserProvider = (props) => {
       const user = await response.json();
       setAuthUser(user);
       setCredentials(credentials);
-      sessionStorage.setItem("authUser", JSON.stringify(user));
-      sessionStorage.setItem("credentials", JSON.stringify(credentials));
+      localStorage.setItem("authUser", JSON.stringify(user));
+      localStorage.setItem("credentials", JSON.stringify(credentials));
       return user;
     } else if (response.status === 401) {
       return null;
@@ -34,7 +34,7 @@ export const UserProvider = (props) => {
   const signOutUser = () => {
     setAuthUser(null);
     setCredentials(null);
-    sessionStorage.clear();
+    localStorage.clear();
   };
 
   return (
